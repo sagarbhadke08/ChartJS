0bc80765c2a4aecf8f543233bfbe40418dd4e0dc
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-03-08 21:05:19 +0530

    FD

---
+++ dashboard/src/Components/Chart.jsx
@@ -0,0 +1,10 @@
+const { useState } = require("react");
+
+const DynamicChart = () => {
+    const [chartData, setChartData] = useState({});
+    const [employeSalary, setEmployeSalary] = useState([]);
+    const [employeAge, setEmployeAge] = useState([]);
+    const chart = () => { }
+}
+
+export default DynamicChart;
